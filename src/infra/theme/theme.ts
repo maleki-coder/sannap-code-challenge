46140b19e019c4321e504934bb0fc03fb1a2b016
mohammad maleki <mohammadmaleki465@gmail.com>
2024-12-13 02:20:49 +0330

    removed unnecessary theme styles

--- src/infra/theme/theme.ts
+++ src/infra/theme/theme.ts
@@ -20,7 +20,7 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
             white: "#ffffff",
           },
           background: {
-            default: mode === "light" ? "#fafafa" : "#121212", // Different backgrounds for each mode
+            default: mode === "light" ? "#fafafa" : "#121212",
             paper: mode === "light" ? "#fff" : "#424242",
           },
           ...(mode === "light"
@@ -76,117 +76,27 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
               }
             : {
                 primary: {
-                  main: "#90caf9", // Define dark mode primary color
+                  main: "#90caf9",
                   contrastText: "#000000",
                 },
                 secondary: {
-                  main: "#ff4081", // Define dark mode secondary color
+                  main: "#ff4081",
                 },
                 error: {
-                  main: "#ef5350", // Define dark mode error color
+                  main: "#ef5350",
                 },
                 warning: {
-                  main: "#ffa726", // Define dark mode warning color
+                  main: "#ffa726",
                 },
                 info: {
-                  main: "#29b6f6", // Define dark mode info color
+                  main: "#29b6f6",
                 },
                 success: {
-                  main: "#66bb6a", // Define dark mode success color
+                  main: "#66bb6a",
                 },
               }),
         },
         components: {
-          MuiSwitch: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                height: "47.5px",
-                "& .MuiSwitch-switchBase": {
-                  transform: "translateX(8px)",
-                  "&.Mui-checked": {
-                    transform: "translateX(20px)",
-                    "& + .MuiSwitch-track": {
-                      border: `3px solid ${theme.palette["customGray"].main}`,
-                    },
-                    "& .MuiSwitch-thumb ": {
-                      color: theme.palette["customGray"].main,
-                      width: "12px",
-                      height: "12px",
-                      marginTop: "8.5px",
-                    },
-                  },
-                  "&:hover": {
-                    background: "none",
-                  },
-                  "& .MuiSwitch-thumb ": {
-                    color: theme.palette["customGreen2"].main,
-                    width: "12px",
-                    height: "12px",
-                    marginTop: "8.5px",
-                    "&:hover": {
-                      boxShadow: "none", // Adjust the shadow on hover
-                    },
-                  },
-                },
-                "& .MuiSwitch-track": {
-                  borderRadius: "60px",
-                  border: `3px solid ${theme.palette["customGreen2"].main}`,
-                  backgroundColor: "transparent",
-                  opacity: 1,
-                },
-              }),
-            },
-          },
-          MuiAccordion: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                backgroundColor: theme.palette["customGray"].lighter,
-                boxShadow: "none",
-                border: "none",
-              }),
-            },
-          },
-          MuiAccordionDetails: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                padding: 0,
-                paddingTop: theme.spacing(1),
-              }),
-            },
-          },
-          MuiAccordionSummary: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                minHeight: "48px",
-                "&.Mui-expanded": {
-                  minHeight: "48px",
-                },
-                backgroundColor: theme.palette["customGray"].lighter_1,
-                color: theme.palette.primary.main,
-                borderRadius: "8px",
-                margin: 0,
-                "& .MuiAccordionSummary-content ": {
-                  "&.Mui-expanded": {
-                    margin: 0,
-                  },
-                },
-              }),
-            },
-          },
-          MuiTab: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                border: "none",
-                "& .MuiTouchRipple-root": {
-                  color: theme.palette["customGreen"].main,
-                },
-                "&.Mui-selected": {
-                  color: theme.palette["customGreen"].main,
-                  borderColor: theme.palette["customGreen"].main,
-                },
-              }),
-            },
-          },
           MuiCircularProgress: {
             styleOverrides: {
               root: ({ theme }) => ({
@@ -194,25 +104,14 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
               }),
             },
           },
-          // MuiFormControlLabel: {
-          //   styleOverrides: {
-          //     root: ({ theme }) => ({
-          //       marginLeft: "0px",
-          //       marginRight: "0px",
-          //       "& .MuiFormControlLabel-label": {
-          //         fontSize: theme.spacing(1.3),
-          //       },
-          //     }),
-          //   },
-          // },
           MuiRadio: {
             styleOverrides: {
               root: ({ theme }) => ({
                 padding: 0,
                 paddingInlineEnd: theme.spacing(1),
-                color: theme.palette["customGray"].main, // Default color of the radio button
+                color: theme.palette["customGray"].main,
                 "&.Mui-checked": {
-                  color: theme.palette["customRed"].main, // Color when checked
+                  color: theme.palette["customRed"].main,
                 },
               }),
             },
@@ -223,7 +122,7 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
                 color: theme.palette.primary.darkChannel,
                 fontSize: "0.8rem",
                 "& .Mui-focused": {
-                  color: theme.palette.secondary.darkChannel, // Color when focused
+                  color: theme.palette.secondary.darkChannel,
                 },
               }),
             },
@@ -239,19 +138,6 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
                 fontSize: "0.8rem",
                 "&.Mui-disabled": {
                   color: theme.palette["customGray"].light,
-                  // "&::before": {
-                  //     content: '""', // Empty string to enable ::before usage
-                  //     display: "block",
-                  //     position: "absolute",
-                  //     top: "50%",
-                  //     left: "50%",
-                  //     transform: "translate(-50%, -50%)",
-                  //     zIndex: 1, // Bring the icon to the front
-                  //     width: "24px", // Icon size
-                  //     height: "24px",
-                  //     background: `url(/assets/images/loginIllustration.png) no-repeat center`, // You can replace `BlockIcon` with any other SVG or image
-                  //     backgroundSize: "cover",
-                  // },
                 },
                 variants: [
                   {
@@ -261,129 +147,34 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
                       color: theme.palette.primary.main,
                     },
                   },
-                  {
-                    props: { variant: "customOutlined" },
-                    style: {
-                      backgroundColor: theme.palette.primary.main,
-                      border: `1px solid ${theme.palette["dayGreen"].main}`,
-                      color: theme.palette["dayGreen"].main,
-                    },
-                  },
-                  {
-                    props: { variant: "customOrange" },
-                    style: {
-                      backgroundColor: theme.palette["customOrange"].main,
-                      color: theme.palette.primary.main,
-                    },
-                  },
-                  {
-                    props: { variant: "customBrown" },
-                    style: {
-                      backgroundColor: theme.palette["customBrown"].main,
-                      color: theme.palette.primary.main,
-                    },
-                  },
-                  {
-                    props: { variant: "customGreen2" },
-                    style: {
-                      backgroundColor: theme.palette["customGreen2"].main,
-                      color: theme.palette.primary.main,
-                    },
-                  },
                 ],
-              }),
-            },
-          },
-          MuiMenuItem: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                color: theme.palette["customBlack"].main,
-                padding: theme.spacing(1.3),
-                marginTop: theme.spacing(0.2),
-                marginBottom: theme.spacing(0.2),
-                fontSize: "0.8rem",
-                "&:hover": {
-                  backgroundColor: theme.palette["customGreen"].main,
-                  color: theme.palette.primary.main,
-                },
-                "&.Mui-selected": {
-                  backgroundColor: theme.palette["customGreen"].main,
-                  color: theme.palette.primary.main,
-                  "&:hover": {
-                    backgroundColor: theme.palette["customGreen"].main,
-                  },
-                },
-              }),
-            },
-          },
-          MuiList: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                borderRadius: "10px",
-                fontSize: "0.2rem",
-              }),
-            },
-          },
-          MuiListItemText: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                "& .MuiListItemText-primary": {
-                  fontSize: "0.8rem",
-                },
-              }),
-            },
-          },
-          MuiListItemIcon: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                color: theme.palette["customBlack"].main,
-                minWidth: `${theme.spacing(3.8)} !important`,
-              }),
-            },
-          },
-          MuiCheckbox: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                "& .MuiSvgIcon-root": {
-                  // Change the border color when unchecked
-                  color: theme.palette["customGray"].main,
-                },
-                "&.Mui-checked .MuiSvgIcon-root": {
-                  // Change the border or icon color when checked
-                  color: theme.palette["customRed"].main, // For checked state
-                },
               }),
             },
           },
           MuiFilledInput: {
             styleOverrides: {
               root: ({ theme }) => ({
-                backgroundColor: theme.palette.primary.main, // Same background color as OutlinedInput
-                borderRadius: "24px", // Custom border radius like OutlinedInput
+                backgroundColor: theme.palette.primary.main,
+                borderRadius: "24px",
                 height: theme.spacing(5),
                 color: theme.palette["customBlack"].main,
                 "&:hover": {
-                  backgroundColor: theme.palette.primary.light, // Hover effect like in OutlinedInput
+                  backgroundColor: theme.palette.primary.light,
                 },
                 "&.Mui-focused": {
-                  backgroundColor: theme.palette.primary.main, // Focus state
+                  backgroundColor: theme.palette.primary.main,
                   borderColor: theme.palette["customBrown"].main,
                 },
                 "&::before": {
-                  borderBottom: `2px solid ${theme.palette["customBrown"].main}`, // Bottom border for FilledInput (before focus)
+                  borderBottom: `2px solid ${theme.palette["customBrown"].main}`,
                 },
                 "&::after": {
-                  borderBottom: `2px solid ${theme.palette["customBrown"].main}`, // Bottom border for FilledInput (after focus)
+                  borderBottom: `2px solid ${theme.palette["customBrown"].main}`,
                 },
               }),
               input: {
-                padding: "12px 14px", // Same padding as OutlinedInput
+                padding: "12px 14px",
               },
-            },
-          },
-          MuiInputBase: {
-            styleOverrides: {
-              root: () => ({}),
             },
           },
           MuiOutlinedInput: {
@@ -391,17 +182,12 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
               root: ({ theme }) => ({
                 backgroundColor: theme.palette.primary.main,
                 borderRadius: theme.spacing(1),
-                // height: theme.spacing(5),
                 fontSize: "0.8rem",
-                // color: theme.palette["customBlack"].main,
-                // "& .MuiOutlinedInput-notchedOutline": {
-                //     paddingInlineStart: "32px",
-                // },
                 "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-                  borderColor: theme.palette["customBrown"].main, // Border color when focused
+                  borderColor: theme.palette["customBrown"].main,
                 },
                 "&:hover .MuiOutlinedInput-notchedOutline": {
-                  borderColor: theme.palette["customBrown"].main, // Change this to your desired hover color
+                  borderColor: theme.palette["customBrown"].main,
                 },
               }),
             },
@@ -409,7 +195,6 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
           MuiFormHelperText: {
             styleOverrides: {
               root: ({ theme }) => ({
-                // fontSize: "12px",
                 color: theme.palette["customBlack"].main,
               }),
             },
@@ -419,19 +204,8 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
               root: ({ theme }) => ({
                 color: theme.palette["customBlack"].main,
                 paddingInlineStart: theme.spacing(0.2),
-                // fontSize : '0.75rem',
                 "&.Mui-focused": {
                   color: theme.palette["customBlack"].main,
-                },
-              }),
-            },
-          },
-          MuiBreadcrumbs: {
-            styleOverrides: {
-              root: ({ theme }) => ({
-                ".MuiBreadcrumbs-separator": {
-                  marginLeft: theme.spacing(0.5),
-                  marginRight: theme.spacing(0.5),
                 },
               }),
             },
@@ -439,17 +213,17 @@ export const getTheme = (mode: "light" | "dark", locale: MUILocaleData) => {
           MuiCssBaseline: {
             styleOverrides: {
               "input:-webkit-autofill": {
-                "-webkit-box-shadow": "0 0 0 50px white inset", // Change to your desired background color
+                "-webkit-box-shadow": "0 0 0 50px white inset",
                 "-webkit-text-fill-color": "#333",
               },
               "input:-webkit-autofill:focus": {
-                "-webkit-box-shadow": "0 0 0 50px white inset", // Change to your desired box-shadow
+                "-webkit-box-shadow": "0 0 0 50px white inset",
                 "-webkit-text-fill-color": "#333",
               },
               body: {
                 "&::-webkit-scrollbar": {
-                  width: "10px", // Scrollbar width
-                  height: "10px", // Scrollbar width
+                  width: "10px",
+                  height: "10px",
                 },
                 "&::-webkit-scrollbar-thumb": {
                   backgroundColor: mode === "dark" ? "#444" : "#888", // Thumb color
